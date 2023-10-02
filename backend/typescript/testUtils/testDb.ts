1f3378da2ec8342f8da56eb0414eee46d21178bf
William Tran <williambrandontran@gmail.com>
2023-10-02 19:07:07 -0400

    init

---
+++ backend/typescript/testUtils/testDb.ts
@@ -0,0 +1,16 @@
+import { resolve } from "path";
+
+import { Sequelize } from "sequelize-typescript";
+
+
+const DATABASE_URL =
+  process.env.NODE_ENV === "production"
+    ? /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
+      process.env.DATABASE_URL!
+    : `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.POSTGRES_DB_TEST}`;
+
+/* eslint-disable-next-line import/prefer-default-export */
+export const testSql = new Sequelize(DATABASE_URL, {
+  models: [resolve(__dirname, "../models/*.model.ts")],
+  logging: false,
+});
