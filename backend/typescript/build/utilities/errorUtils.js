d98ea475fd208e498abcccebf3f65bf9cccd9ef9
William Tran <williambrandontran@gmail.com>
2023-10-14 15:54:30 -0400

    fix: update gitignore, remove build

--- backend/typescript/build/utilities/errorUtils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getErrorMessage = void 0;
-/* eslint-disable-next-line import/prefer-default-export */
-const getErrorMessage = (error) => {
-    return error instanceof Error ? error.message : "Unknown error occurred.";
-};
-exports.getErrorMessage = getErrorMessage;
