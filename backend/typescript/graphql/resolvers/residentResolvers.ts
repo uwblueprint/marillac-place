7841fb1fbfd02a3ea773d0f48a0a23b8895318d4
Nandini Mehrotra <90271211+NandiniMeh@users.noreply.github.com>
2023-11-18 12:12:52 -0500

    feat: create interface and types for resident (#18)
    * feat: create interface and types for resident
    
    * fix: add newline to end of file
    
    * addressing PR comments - minor changes to resident interface
    
    * feat: create residentResolvers, file for impl
    
    * add notification and notification_user models (#17)
    
    * feat: create impl file, add prisma client
    
    * Updates to Resident Service and Types
    
    * feat: add logging for errors
    
    * feat: create interface and types for resident
    
    * fix: add newline to end of file
    
    * addressing PR comments - minor changes to resident interface
    
    * feat: create residentResolvers, file for impl
    
    * feat: create impl file, add prisma client
    
    * Updates to Resident Service and Types
    
    * feat: add logging for errors
    
    * fix: remove duplicate declarations
    
    * feat: add neon db (#21)
    
    * feat: add neon db
    
    * fix: add optional neon setup
    
    * feat: add logging for errors
    
    * upgrade: node and ts dependancies
    
    * feat: fix linting and add schema mapping
    
    * fix: recurrence freq naming
    
    * Add graph ql custom scalar data types
    
    * Update Eslinting modules
    
    * fix: address PR comments
    
    ---------
    
    Co-authored-by: Selena Zhang <selenazhang305@gmail.com>
    Co-authored-by: Danie1T <82688216+Danie1T@users.noreply.github.com>
    Co-authored-by: Sharujan Sreekaran <ohnose123@gmail.com>
    Co-authored-by: William Tran <41273929+williamtran10@users.noreply.github.com>
    Co-authored-by: William Tran <williambrandontran@gmail.com>

---
+++ backend/typescript/graphql/resolvers/residentResolvers.ts
@@ -0,0 +1,54 @@
+import ResidentService from "../../services/implementations/residentService";
+import type {
+  IResidentService,
+  ResidentDTO,
+  CreateResidentDTO,
+  UpdateResidentDTO,
+} from "../../services/interfaces/residentService";
+
+const residentService: IResidentService = new ResidentService();
+// const authService: IAuthService = new AuthService(userService, emailService);
+
+const residentResolvers = {
+  Query: {
+    residentsById: async (
+      _parent: undefined,
+      { id }: { id: string[] },
+    ): Promise<Array<ResidentDTO>> => {
+      return residentService.getResidentsById(id.map(Number));
+    },
+    allResidents: async (): Promise<Array<ResidentDTO>> => {
+      return residentService.getAllResidents();
+    },
+  },
+  Mutation: {
+    addResident: async (
+      _parent: undefined,
+      { resident }: { resident: CreateResidentDTO },
+    ): Promise<ResidentDTO> => {
+      const newResident = await residentService.addResident(resident);
+      return newResident;
+    },
+    updateResident: async (
+      _parent: undefined,
+      { id, resident }: { id: string; resident: UpdateResidentDTO },
+    ): Promise<ResidentDTO> => {
+      const newResident = await residentService.updateResident(
+        parseInt(id, 10),
+        resident,
+      );
+      return newResident;
+    },
+    deleteResident: async (
+      _parent: undefined,
+      { id }: { id: string },
+    ): Promise<ResidentDTO> => {
+      const deletedResident = await residentService.deleteResident(
+        parseInt(id, 10),
+      );
+      return deletedResident;
+    },
+  },
+};
+
+export default residentResolvers;
