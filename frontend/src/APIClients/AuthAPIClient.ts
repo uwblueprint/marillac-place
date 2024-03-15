f7764e062039bb3cd7c5710c980f73893942f344
cindyhjli <cindy.hongjia.li16@gmail.com>
2024-03-14 22:26:20 -0400

    fixed login page and added logo

--- frontend/src/APIClients/AuthAPIClient.ts
+++ frontend/src/APIClients/AuthAPIClient.ts
@@ -25,16 +25,16 @@ const login = async (
   loginFunction: LoginFunction,
 ): Promise<AuthenticatedUser | null> => {
   let user: AuthenticatedUser = null;
-  try {
-    const result = await loginFunction({ variables: { email, password } });
-    user = result.data?.login ?? null;
-    if (user) {
-      localStorage.setItem(AUTHENTICATED_USER_KEY, JSON.stringify(user));
-    }
-  } catch (e: unknown) {
-    // eslint-disable-next-line no-alert
-    window.alert("Failed to login");
+  // try {
+  const result = await loginFunction({ variables: { email, password } });
+  user = result.data?.login ?? null;
+  if (user) {
+    localStorage.setItem(AUTHENTICATED_USER_KEY, JSON.stringify(user));
   }
+  // } catch (e: unknown) {
+  //   // eslint-disable-next-line no-alert
+  //   window.alert("Failed to login");
+  // }
   return user;
 };
 
--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,40 +1,29 @@
 import React, { useContext, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
-import {
-  GoogleLogin,
-  GoogleLoginResponse,
-  GoogleLoginResponseOffline,
-} from "react-google-login";
 import { gql, useMutation } from "@apollo/client";
+
+import {
+  Button,
+  Box,
+  Text,
+  Input,
+  FormControl,
+  FormErrorMessage
+} from "@chakra-ui/react";
+
 
 import authAPIClient from "../../APIClients/AuthAPIClient";
 import { HOME_PAGE, SIGNUP_PAGE } from "../../constants/Routes";
 import AuthContext from "../../contexts/AuthContext";
 import { AuthenticatedUser } from "../../types/AuthTypes";
+import { ReactComponent as Logo } from '../../assets/Marillac_Place_Logo.svg';
 
-type GoogleResponse = GoogleLoginResponse | GoogleLoginResponseOffline;
 
-type GoogleErrorResponse = {
-  error: string;
-  details: string;
-};
+
 
 const LOGIN = gql`
   mutation Login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-      id
-      firstName
-      lastName
-      email
-      role
-      accessToken
-    }
-  }
-`;
-
-const LOGIN_WITH_GOOGLE = gql`
-  mutation LoginWithGoogle($idToken: String!) {
-    loginWithGoogle(idToken: $idToken) {
       id
       firstName
       lastName
@@ -50,94 +39,134 @@ const Login = (): React.ReactElement => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const [error, setError] = useState("");
 
   const [login] = useMutation<{ login: AuthenticatedUser }>(LOGIN);
-  const [loginWithGoogle] = useMutation<{ loginWithGoogle: AuthenticatedUser }>(
-    LOGIN_WITH_GOOGLE,
-  );
 
   const onLogInClick = async () => {
-    const user: AuthenticatedUser = await authAPIClient.login(
-      email,
-      password,
-      login,
-    );
-    setAuthenticatedUser(user);
+    try {
+      const user: AuthenticatedUser = await authAPIClient.login(
+        email,
+        password,
+        login,
+      );
+      setAuthenticatedUser(user);
+      setError('');
+    } catch (e: unknown) {
+      setError('Wrong email or password. Try again.');  
+    }
   };
 
   const onSignUpClick = () => {
     navigate(SIGNUP_PAGE);
   };
 
-  const onGoogleLoginSuccess = async (idToken: string) => {
-    const user: AuthenticatedUser = await authAPIClient.loginWithGoogle(
-      idToken,
-      loginWithGoogle,
-    );
-    setAuthenticatedUser(user);
-  };
+// CHANGE TO RESET_PASSWORD_PAGE 
+  const onForgotPasswordClick = () => {
+    navigate(SIGNUP_PAGE)
+  }
 
   if (authenticatedUser) {
     return <Navigate to={HOME_PAGE} />;
-  }
+  } 
 
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h1>Login</h1>
-      <form>
-        <div>
-          <input
+
+return (
+  <Box width="100%" display="flex" flexDirection="column" alignContent="center" minH="100vh">
+    <Box paddingLeft="38px"
+    paddingTop="34px">
+      <Logo/>
+    </Box>
+    
+    <Box maxWidth="514px" height="480px" mt="8vh" mx="auto" padding="48px" borderRadius="8px" boxShadow="0 5px 15px 1px rgba(0, 0, 0, 0.09)">
+      <Text fontSize="38px" fontWeight="bold" mb="8px">
+        Sign In
+      </Text>
+      <Text fontSize="18px" mb="48px">
+        Please enter your login information
+      </Text>
+      <FormControl isInvalid={!!error} alignItems="center">
+          <Input
+            id="email"
             type="email"
             value={email}
+            height="49px"
             onChange={(event) => setEmail(event.target.value)}
-            placeholder="username@domain.com"
+            placeholder="Email or Phone Number"
+            borderColor="black"
+            borderWidth="2px"
+            mb="32px"
+            fontSize="18px"
+            paddingTop={email ? '14px' : '0'}
           />
-        </div>
-        <div>
-          <input
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            placeholder="password"
-          />
-        </div>
-        <div>
-          <button
-            className="btn btn-primary"
-            type="button"
-            onClick={onLogInClick}
-          >
-            Log In
-          </button>
-        </div>
-        <GoogleLogin
-          clientId={process.env.REACT_APP_OAUTH_CLIENT_ID || ""}
-          buttonText="Login with Google"
-          onSuccess={(response: GoogleResponse): void => {
-            if ("tokenId" in response) {
-              onGoogleLoginSuccess(response.tokenId);
-            } else {
-              // eslint-disable-next-line no-alert
-              window.alert(response);
-            }
-          }}
-          onFailure={(error: GoogleErrorResponse) =>
-            // eslint-disable-next-line no-alert
-            window.alert(JSON.stringify(error))
-          }
-        />
-      </form>
-      <div>
-        <button
-          className="btn btn-primary"
-          type="button"
-          onClick={onSignUpClick}
+       {email !== '' && (
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          paddingLeft="17px"
+          paddingTop="2px"
+          fontSize="sm"
+          color="black"
         >
-          Sign Up
-        </button>
-      </div>
-    </div>
-  );
+          Email
+        </Box>
+       )}
+        
+        <Input
+          id="password"
+          type="password"
+          value={password}
+          height="49px"
+          onChange={(event) => setPassword(event.target.value)}
+          borderColor="black"
+          borderWidth="2px"
+          fontSize="18px"
+          placeholder="Password"
+        />
+        
+        <FormErrorMessage
+        fontSize="18px"
+        >
+          {error}</FormErrorMessage>
+        <Text
+        onClick={onForgotPasswordClick}
+        cursor="pointer"
+        fontWeight="bold"
+        fontSize="18px"
+        textDecoration="underline"
+        float="right"
+        mt="8px"
+      >
+        Forgot Password?
+      </Text>
+        <Button onClick={onLogInClick} mt="10px" paddingTop="15px" width="418px" height="60px" backgroundColor="purple" color="white" borderRadius="48px">
+          <Text
+          fontSize="24px"
+          >
+            Sign In
+          </Text>
+          
+        </Button>
+      </FormControl>
+    </Box>
+    <Box mx="auto" mt="24px">
+      <Text as="span" fontSize="18px">
+        Don&apos;t have an account?&nbsp;
+      </Text>
+      <Text as="span"
+        fontWeight="bold"
+        textDecoration="underline"
+        textColor="purple"
+        fontSize="18px"
+        onClick={onSignUpClick}
+        cursor="pointer"
+      >
+         Join Now
+      </Text>
+    </Box>
+  </Box>
+);
 };
 
 export default Login;
