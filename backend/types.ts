1c7852573212467ad796cf078db50f132dbed80a
William Park <w22park@uwaterloo.ca>
2024-03-21 01:20:02 -0400

    Remove commented backend types

--- backend/types.ts
+++ backend/types.ts
@@ -1,27 +1,7 @@
-// export type UserType = "STAFF" | "RESIDENT";
-
 export type Token = {
   accessToken: string;
   refreshToken: string;
 };
-
-// export type UserDTO = {
-//   id: number;
-//   firstName: string;
-//   lastName: string;
-//   email: string;
-//   type: UserType;
-// };
-
-// export type CreateUserDTO = Omit<UserDTO, "id"> & { password: string };
-
-// export type UpdateUserDTO = Omit<UserDTO, "id">;
-
-// export type RegisterUserDTO = Omit<CreateUserDTO, "role">;
-
-// export type AuthDTO = Token & UserDTO;
-
-// export type Letters = "A" | "B" | "C" | "D";
 
 export type NodemailerConfig = {
   service: "gmail";
