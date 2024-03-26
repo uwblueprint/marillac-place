9b6e86f25615a303603a5f4ca08124cf440f20fb
Jesse Huang <jesshuang5733@gmail.com>
2024-03-25 22:18:15 -0400

    Feat: added selectable boxes

--- frontend/src/components/common/CommonTable.tsx
+++ frontend/src/components/common/CommonTable.tsx
@@ -9,57 +9,95 @@ import {
   Td,
   TableCaption,
   TableContainer,
-  Checkbox
-} from '@chakra-ui/react'
+  Checkbox,
+  Image,
+} from "@chakra-ui/react";
+import pencil from "../../assets/pencil_edit.png";
 
+export type TableTypes = "string" | "number" | "boolean" | "date";
 
-export type tableTypes = "string" | "number" | "boolean" | "date";
+export type ColumnInfoTypes = { Header: string; Type: TableTypes; Key: string };
 
-export type columnInfoType = { Header: string, Type: tableTypes, Key: string }
-
-export type tableProps = {
+export type TableProps = {
   data: any[];
-  columnInfo: columnInfoType[];
+  columnInfo: ColumnInfoTypes[];
   onEdit: (row: any) => any;
   maxResults: number;
   isSelectable: boolean;
-}
+};
 
-const onChangeAll = (type: ChangeEvent<HTMLInputElement>) => {
-  if (type.target.checked) {
-    
-  } else {
+// const onChangeAll = (type: ChangeEvent<HTMLInputElement>) => {
+//   if (type.target.checked) {
 
-  }
-}
+//   } else {
+//   }
+// };
 
-const CommonTable = (props: tableProps): React.ReactElement => {
+const CommonTable = ({
+  columnInfo,
+  data,
+  isSelectable,
+  onEdit,
+}: TableProps): React.ReactElement => {
+  const checkRows: boolean[] = [];
+  Object.keys(data).forEach((item) => {
+    if (Object.prototype.hasOwnProperty.call(data, item)) {
+      checkRows.push(false);
+    }
+  });
+  const [checked, setChecked] = useState(checkRows);
+
+  const allChecked = checked.every(Boolean);
+  const isIndeterminate = checked.some(Boolean) && !allChecked;
 
   return (
     <TableContainer>
       <Table>
         <Thead>
           <Tr>
-          (isSelectable ? <Checkbox></Checkbox> : null)
-          {props.columnInfo.map((header, index) => {
-            return (
-              <Th key={index}>{header.Header}</Th>
-            )
-          })}
+            {isSelectable ? (
+              <Checkbox
+                isChecked={allChecked}
+                isIndeterminate={isIndeterminate}
+                onChange={(e) => {
+                  const newChecked = checked.map(() => e.target.checked);
+                  setChecked(newChecked);
+                }}
+              />
+            ) : null}
+            {columnInfo.map((header, index) => {
+              return <Th key={index}>{header.Header}</Th>;
+            })}
           </Tr>
         </Thead>
         <Tbody>
-          {props.data.map((row, index) => {
+          {data.map((row, index) => {
             return (
               <Tr key={index}>
-                {props.columnInfo.map((column, index) => {
-                  return (
-                    <Td key={index}>{row[column.Key as keyof typeof row]}</Td>
-                  )
+                {isSelectable ? (
+                  <Checkbox
+                    isChecked={checked[index]}
+                    onChange={(e) => {
+                      const newChecked = [...checked];
+                      newChecked[index] = e.target.checked;
+                      setChecked(newChecked);
+                    }}
+                  />
+                ) : null}
+                {columnInfo.map((column, i) => {
+                  return <Td key={i}>{row[column.Key as keyof typeof row]}</Td>;
                 })}
-                <Td onClick={() => props.onEdit(row)}>EDIT</Td>
+                <Td onClick={() => onEdit(row)}>
+                  <Image
+                    boxSize="25px"
+                    objectFit="cover"
+                    _hover={{ cursor: "pointer" }}
+                    src={pencil}
+                    alt="edit"
+                  />
+                </Td>
               </Tr>
-            )
+            );
           })}
         </Tbody>
       </Table>
--- frontend/src/components/pages/Default.tsx
+++ frontend/src/components/pages/Default.tsx
@@ -6,57 +6,60 @@ import SampleContext from "../../contexts/SampleContext";
 import Logout from "../auth/Logout";
 import RefreshCredentials from "../auth/RefreshCredentials";
 import ResetPassword from "../auth/ResetPassword";
-import CommonTable, { columnInfoType, tableProps } from "../common/CommonTable";
+import CommonTable, {
+  ColumnInfoTypes,
+  TableProps,
+} from "../common/CommonTable";
 
 const mockData = [
   {
-    "id": 1,
-    "title": "taxes",
-    "description": "do ur taxes",
-    "room": 1,
-    "credits": 3.0,
-    "due_date": "2022-12-12",
+    id: 1,
+    title: "taxes",
+    description: "do ur taxes",
+    room: 1,
+    credits: "$4.00",
+    due_date: "2022-12-12",
   },
   {
-    "id": 2,
-    "title": "taxes",
-    "description": "do ur taxes",
-    "room": 1,
-    "credits": 3.0,
-    "due_date": "2022-12-12",
+    id: 2,
+    title: "more taxes",
+    description: "do ur taxes",
+    room: 2,
+    credits: "$3.00",
+    due_date: "2022-12-12",
   },
   {
-    "id": 3,
-    "title": "taxes",
-    "description": "do ur taxes",
-    "room": 1,
-    "credits": 4.0,
-    "due_date": "2022-12-12",
-  }
-]
+    id: 3,
+    title: "heap of taxes",
+    description: "do ur taxes",
+    room: 3,
+    credits: "$2.00",
+    due_date: "2022-12-12",
+  },
+];
 
-const mockColumns: columnInfoType[] = [
+const mockColumns: ColumnInfoTypes[] = [
   {
     Header: "Task name",
     Type: "string",
-    Key: "title"
+    Key: "title",
   },
   {
     Header: "Room Number",
     Type: "number",
-    Key: "room"
+    Key: "room",
   },
   {
     Header: "Due Date",
     Type: "date",
-    Key: "due_date"
+    Key: "due_date",
   },
   {
     Header: "Requested Marillac Bucks",
     Type: "number",
-    Key: "credits"
-  }
-]
+    Key: "credits",
+  },
+];
 
 type ButtonProps = { text: string; path: string };
 
@@ -89,8 +92,8 @@ const TeamInfoDisplay = () => {
 };
 
 const testEdit = () => {
-  console.log("edit clicked")
-}
+  alert("AYO YOU EDITING RN");
+};
 
 const Default = (): React.ReactElement => {
   return (
@@ -121,7 +124,13 @@ const Default = (): React.ReactElement => {
 
       <div style={{ height: "2rem" }} />
 
-      <CommonTable columnInfo={mockColumns} data={mockData} onEdit={() => testEdit()} maxResults={8} isSelectable={true}/>
+      <CommonTable
+        columnInfo={mockColumns}
+        data={mockData}
+        onEdit={() => testEdit()}
+        maxResults={8}
+        isSelectable
+      />
       <TeamInfoDisplay />
     </div>
   );
