89c18bbce469fc76ca01cc22e2d8e8b61c88da6c
William Park <w22park@uwaterloo.ca>
2024-04-10 18:56:01 -0400

    Reduce repeated code in ParticipantsModal

--- frontend/src/components/pages/participants/ParticipantsModal.tsx
+++ frontend/src/components/pages/participants/ParticipantsModal.tsx
@@ -19,6 +19,62 @@ type Props = {
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
+const FormField = ({
+  label,
+  value,
+  type = "text",
+  onChange,
+  submitPressed,
+  required = false,
+  isPassword = false,
+  showPassword,
+  setShowPassword,
+}: {
+  label: string;
+  value: string;
+  type?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  submitPressed: boolean;
+  required?: boolean;
+  isPassword?: boolean;
+  showPassword?: boolean;
+  setShowPassword?: React.Dispatch<React.SetStateAction<boolean>>;
+}) => (
+  <Flex flexDir="column" flex="1">
+    <FormControl isRequired={required}>
+      <FormLabel marginBottom="5px" color="gray.main" fontWeight="700">
+        {label}
+      </FormLabel>
+      <InputGroup>
+        <Input
+          variant="primary"
+          borderColor={submitPressed && !value ? "red.error" : "gray.300"}
+          boxShadow={submitPressed && !value ? "0 0 2px red.error" : "none"}
+          type={
+            isPassword && setShowPassword && !showPassword ? "password" : type
+          }
+          value={value}
+          onChange={onChange}
+        />
+        {isPassword && setShowPassword && (
+          <InputRightElement height="34px">
+            <Button
+              onClick={() => setShowPassword(!showPassword)}
+              variant="icon"
+            >
+              {!showPassword ? (
+                <VisibilityIcon fontSize="small" />
+              ) : (
+                <VisibilityOffIcon fontSize="small" />
+              )}
+            </Button>
+          </InputRightElement>
+        )}
+      </InputGroup>
+    </FormControl>
+  </Flex>
+);
+
 const ParticipantsModal = ({
   isOpen,
   setIsOpen,
@@ -76,174 +132,74 @@ const ParticipantsModal = ({
     >
       <Flex flexDirection="column" gap="20px">
         <Flex gap="20px">
-          <Flex flexDir="column" flex="1">
-            <FormControl isRequired>
-              <FormLabel marginBottom="5px" color="gray.main" fontWeight="700">
-                First name
-              </FormLabel>
-              <Input
-                variant="primary"
-                borderColor={
-                  submitPressed && !firstName ? "red.error" : "gray.300"
-                }
-                boxShadow={
-                  submitPressed && !firstName ? "0 0 2px red.error" : "none"
-                }
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-            </FormControl>
-          </Flex>
-
-          <Flex flexDir="column" flex="1">
-            <FormControl isRequired>
-              <FormLabel marginBottom="5px" color="gray.main" fontWeight="700">
-                Last name
-              </FormLabel>
-              <Input
-                variant="primary"
-                borderColor={
-                  submitPressed && !lastName ? "red.error" : "gray.300"
-                }
-                boxShadow={
-                  submitPressed && !lastName ? "0 0 2px red.error" : "none"
-                }
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-              />
-            </FormControl>
-          </Flex>
+          <FormField
+            label="First name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            submitPressed={submitPressed}
+            required
+          />
+          <FormField
+            label="Last name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            submitPressed={submitPressed}
+            required
+          />
         </Flex>
-
         <Flex gap="20px">
-          <Flex flexDir="column" flex="1">
-            <FormControl isRequired>
-              <FormLabel marginBottom="5px" color="gray.main" fontWeight="700">
-                Email
-              </FormLabel>
-              <Input
-                variant="primary"
-                borderColor={submitPressed && !email ? "red.error" : "gray.300"}
-                boxShadow={
-                  submitPressed && !email ? "0 0 2px red.error" : "none"
-                }
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </FormControl>
-          </Flex>
-
-          <Flex flexDir="column" flex="1">
-            <FormControl isRequired>
-              <FormLabel marginBottom="5px" color="gray.main" fontWeight="700">
-                Phone Number
-              </FormLabel>
-              <Input
-                variant="primary"
-                borderColor={
-                  submitPressed && !phoneNumber ? "red.error" : "gray.300"
-                }
-                boxShadow={
-                  submitPressed && !phoneNumber ? "0 0 2px red.error" : "none"
-                }
-                type="tel"
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-              />
-            </FormControl>
-          </Flex>
+          <FormField
+            label="Email"
+            value={email}
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            submitPressed={submitPressed}
+            required
+          />
+          <FormField
+            label="Phone Number"
+            value={phoneNumber}
+            type="tel"
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            submitPressed={submitPressed}
+            required
+          />
         </Flex>
-
-        <Flex>
-          <FormControl isRequired>
-            <FormLabel marginBottom="5px" color="gray.main" fontWeight="700">
-              ID
-            </FormLabel>
-            <Input
-              variant="primary"
-              borderColor={
-                submitPressed && !residentId ? "red.error" : "gray.300"
-              }
-              boxShadow={
-                submitPressed && !residentId ? "0 0 2px red.error" : "none"
-              }
-              value={residentId}
-              onChange={(e) => setResidentId(e.target.value)}
-            />
-          </FormControl>
-        </Flex>
-
-        <Flex>
-          <FormControl isRequired>
-            <FormLabel marginBottom="5px" color="gray.main" fontWeight="700">
-              Password
-            </FormLabel>
-            <InputGroup>
-              <Input
-                variant="primary"
-                borderColor={
-                  submitPressed && !password ? "red.error" : "gray.300"
-                }
-                boxShadow={
-                  submitPressed && !password ? "0 0 2px red.error" : "none"
-                }
-                type={showPassword ? "text" : "password"}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <InputRightElement height="34px">
-                <Button
-                  onClick={() => setShowPassword(!showPassword)}
-                  variant="icon"
-                >
-                  {!showPassword ? (
-                    <VisibilityIcon fontSize="small" />
-                  ) : (
-                    <VisibilityOffIcon fontSize="small" />
-                  )}
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-          </FormControl>
-        </Flex>
-
+        <FormField
+          label="ID"
+          value={residentId}
+          type="number"
+          onChange={(e) => setResidentId(e.target.value)}
+          submitPressed={submitPressed}
+          required
+        />
+        <FormField
+          label="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          submitPressed={submitPressed}
+          required
+          isPassword
+          showPassword={showPassword}
+          setShowPassword={setShowPassword}
+        />
         <Flex gap="20px">
-          <Flex flexDir="column" flex="1">
-            <FormControl isRequired>
-              <FormLabel marginBottom="5px" color="gray.main" fontWeight="700">
-                Arrival Date
-              </FormLabel>
-              <Input
-                variant="primary"
-                borderColor={
-                  submitPressed && !arrivalDate ? "red.error" : "gray.300"
-                }
-                boxShadow={
-                  submitPressed && !arrivalDate ? "0 0 2px red.error" : "none"
-                }
-                value={arrivalDate}
-                type="date"
-                onChange={(e) => setArrivalDate(e.target.value)}
-              />
-            </FormControl>
-          </Flex>
-
-          <Flex flexDir="column" flex="1">
-            <FormControl>
-              <FormLabel marginBottom="5px" color="gray.main" fontWeight="700">
-                Departure Date
-              </FormLabel>
-              <Input
-                variant="primary"
-                value={departureDate}
-                type="date"
-                onChange={(e) => setDepartureDate(e.target.value)}
-              />
-            </FormControl>
-          </Flex>
+          <FormField
+            label="Arrival Date"
+            value={arrivalDate}
+            type="date"
+            onChange={(e) => setArrivalDate(e.target.value)}
+            submitPressed={submitPressed}
+            required
+          />
+          <FormField
+            label="Departure Date"
+            value={departureDate}
+            type="date"
+            onChange={(e) => setDepartureDate(e.target.value)}
+            submitPressed={submitPressed}
+          />
         </Flex>
-
         <Flex>
           <FormControl>
             <FormLabel marginBottom="5px" color="gray.main" fontWeight="700">
