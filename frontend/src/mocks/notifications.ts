e49c8f870f47bba0cce85e4d58bda43218a5346f
William Park <w22park@uwaterloo.ca>
2024-04-11 05:13:56 -0400

    Announcements mock data

---
+++ frontend/src/mocks/notifications.ts
@@ -0,0 +1,42 @@
+export const announcementsMockData = {
+  "1,2,3": [
+    {
+      author: "John Doe",
+      message:
+        "Reminding you about your meeting this Saturday! Please be on time, we will be beginning promptly at 10:30am. If you are unable to attend, please let us know as soon as possible. Have a great rest of the week everyone!",
+      createdAt: "2023-07-01T00:00:00Z",
+    },
+    {
+      author: "John Doe",
+      message:
+        "Reminder to please clean up after yourself in the kitchen. We have had a few complaints about the mess left behind. Thank you for your cooperation!",
+      createdAt: "2023-08-01T00:00:00Z",
+    },
+    {
+      author: "Jane Doe",
+      message: "Great job on your tasks this week! Keep up the good work!",
+      createdAt: "2023-09-01T00:00:00Z",
+    },
+  ],
+  "4": [
+    {
+      author: "John Doe",
+      message:
+        "Reminding you about your meeting this Saturday! Please be on time, we will be beginning promptly at 10:30am. If you are unable to attend, please let us know as soon as possible. Have a great rest of the week everyone!",
+      createdAt: "2023-07-01T00:00:00Z",
+    },
+    {
+      author: "John Doe",
+      message:
+        "Reminder to please clean up after yourself in the kitchen. We have had a few complaints about the mess left behind. Thank you for your cooperation!",
+      createdAt: "2023-08-01T00:00:00Z",
+    },
+    {
+      author: "Jane Doe",
+      message: "Great job on your tasks this week! Keep up the good work!",
+      createdAt: "2023-09-01T00:00:00Z",
+    },
+  ],
+};
+
+export default {};
