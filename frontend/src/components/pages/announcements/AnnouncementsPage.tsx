230b79cd766c98924442b0cd0a429a27e5588182
KathleenX7 <kxiong1210@gmail.com>
2024-06-05 22:07:50 -0400

    Develop frontend for creating announcements

--- frontend/src/components/pages/announcements/AnnouncementsPage.tsx
+++ frontend/src/components/pages/announcements/AnnouncementsPage.tsx
@@ -9,6 +9,8 @@ import { announcementsMockData } from "../../../mocks/notifications";
 const AnnouncementsPage = (): React.ReactElement => {
   const [announcements, setAnnouncements] = useState<GroupAnnouncements>({});
   const [selectedGroup, setSelectedGroup] = useState<string>("");
+  const [addingNewRoom, setAddingNewRoom] = useState<boolean>(false);
+  const [selectedRooms, setSelectedRooms] = useState<number[]>([]);
 
   useEffect(() => {
     // TODO: Fetch announcements from API
@@ -20,10 +22,17 @@ const AnnouncementsPage = (): React.ReactElement => {
       <AnnouncementsGroups
         announcements={announcements}
         setSelectedGroup={setSelectedGroup}
+        addingNewRoom={addingNewRoom}
+        setAddingNewRoom={setAddingNewRoom}
+        selectedRooms={selectedRooms}
       />
       <AnnouncementsView
         announcements={announcements}
         selectedGroup={selectedGroup}
+        addingNewRoom={addingNewRoom}
+        setAddingNewRoom={setAddingNewRoom}
+        selectedRooms={selectedRooms}
+        setSelectedRooms={setSelectedRooms}
       />
     </Flex>
   );
