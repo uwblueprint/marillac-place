6f6ec57bb6e1288b8bf3a65367637dfa1ed01c26
cindyhjli <cindy.hongjia.li16@gmail.com>
2024-06-05 22:06:50 -0400

    fix date and time

--- frontend/src/components/pages/tasks/TaskModal.tsx
+++ frontend/src/components/pages/tasks/TaskModal.tsx
@@ -15,6 +15,10 @@ import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import { TbTrash } from "react-icons/tb";
 import { format } from 'date-fns';
+import dayjs, { Dayjs } from 'dayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 import ModalContainer from "../../common/ModalContainer";
 
@@ -61,6 +65,7 @@ const FormField = ({
           }
           value={value}
           onChange={onChange}
+          onBlur={onBlur}
         />
         {isPassword && setShowPassword && (
           <InputRightElement h="34px">
@@ -82,6 +87,28 @@ const FormField = ({
   </Flex>
 );
 
+const generateOptions = () => {
+  const options = [];
+  for (let hour = 0; hour < 24; hour+=1) {
+    for (let minute = 0; minute < 60; minute += 30) {
+      let formattedHour;
+      if (hour === 0) {
+        formattedHour = "12";
+      } else if (hour > 12) {
+        formattedHour = `${hour - 12}`;
+      } else{
+        formattedHour = `${hour}`;
+      }
+      const ampm = hour < 12 ? 'am' : 'pm';
+      const formattedMinute = minute.toString().padStart(2, '0');
+      options.push(`${formattedHour}:${formattedMinute}${ampm}`);
+    }
+  }
+  return options;
+};
+
+const options = generateOptions();
+
 const DateInput = ({
   dueDate,
   setDueDate,
@@ -93,8 +120,8 @@ const DateInput = ({
   setRecurrenceFrequency,
   submitPressed,
 }: {
-  dueDate: string;
-  setDueDate: React.Dispatch<React.SetStateAction<string>>;
+  dueDate: Dayjs | null;
+  setDueDate: React.Dispatch<React.SetStateAction<Dayjs | null>>;
   dueTime: string;
   setDueTime: React.Dispatch<React.SetStateAction<string>>;
   isAllDay: boolean;
@@ -103,11 +130,12 @@ const DateInput = ({
   setRecurrenceFrequency: React.Dispatch<React.SetStateAction<string>>;
   submitPressed: boolean;
 }) => {
-    const formatDate = (inputDate: Date) => {
-      return format(inputDate, 'yyyy-MM-dd');
-    };
-  
-    return (
+  // const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  // const [selectedDate, setSelectedDate] = React.useState<Dayjs | null>(dayjs().startOf('day'));
+
+  const [calendarIsOpen, setCalendarIsOpen] = useState(false);
+
+  return(
   <Flex flexDir="column" flex="1">
     <FormControl isRequired>
       <FormLabel mb="5px" color="gray.main" fontWeight="700">
@@ -115,7 +143,8 @@ const DateInput = ({
       </FormLabel>
       <Flex flexDir="column">
         <Flex flexDir="row">
-          <Input
+            {/* date starts */}
+          {/* <Input
             variant="primary"
             mb={3}
             borderColor={submitPressed && !dueDate ? "red.error" : "gray.300"}
@@ -123,12 +152,72 @@ const DateInput = ({
               submitPressed && !dueDate ? "0 0 2px red.error" : "none"
             }
             type="date"
-            value={dueDate ? formatDate(new Date(dueDate)) : ''}
+            value={dueDate}
             width="200px"
             onChange={(e) => setDueDate(e.target.value)}
-          />
+          /> */}
+          {/* <div>
+            <DatePicker 
+              onChange={(date) => {
+                setSelectedDate(date as Date | null);
+                setCalendarIsOpen(false);
+              }}
+              isOpen={calendarIsOpen}
+              value={selectedDate}
+              format="MMM dd, y" 
+              // shouldOpenCalendar={({ reason }) => reason !== 'focus'}
+              // format="yyyy-MM-dd" 
+              // calendarIcon={null}
+              // clearIcon={null}
+              required
+              closeCalendar
+              // shouldOpenCalendar={() => false}
+            />
+          </div> */}
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              // label="Date Picker"
+              // // format="LL"
+              // value={dueDate}
+              // defaultValue={dayjs('2022-04-17')}
+              // onChange={
+              //   (date) => {
+              //     setDueDate(date as Dayjs | null);
+              //   }
+              // }
+              label="Controlled picker"
+              value={dueDate}
+              onChange={(newValue) => setDueDate(newValue)}
+            />
+          </LocalizationProvider>
+
+
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker />
+          </LocalizationProvider>
+          
           <Text paddingX="10px" paddingY="4px">at</Text>
-          <Input
+          <Select
+            variant="primary"
+            value={dueTime}
+            onChange={(e) => setDueTime(e.target.value)}
+            border="solid"
+            borderColor={submitPressed && !dueTime ? "red.error" : "gray.300"}
+            boxShadow={
+              submitPressed && !dueTime ? "0 0 2px red.error" : "none"
+            }
+            borderWidth="2px"
+            height="34px"
+            width="200px"
+          >
+            <option value="">Select...</option>
+            {options.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            ))}
+          </Select>
+          {/* <Input
             variant="primary"
             mb={3}
             borderColor={submitPressed && !dueTime ? "red.error" : "gray.300"}
@@ -139,7 +228,7 @@ const DateInput = ({
             value={dueTime}
             width="200px"
             onChange={(e) => setDueTime(e.target.value)}
-          />
+          /> */}
         </Flex>
         <Flex alignItems="center">
           <Checkbox
@@ -155,6 +244,8 @@ const DateInput = ({
             onChange={(e) => setRecurrenceFrequency(e.target.value)}
             border="solid"
             borderColor="gray.300"
+            borderWidth="2px"
+            height="34px"
           >
             <option value="daily">Daily</option>
             <option value="weekly">Weekly</option>
@@ -170,18 +261,17 @@ const DateInput = ({
 const TaskModal = ({ isOpen, setIsOpen }: Props): React.ReactElement => {
   const [title, setTitle] = useState("");
   const [location, setLocation] = useState("");
-  const [dueDate, setDueDate] = useState("");
+  const [dueDate, setDueDate] = useState<Dayjs | null>(dayjs().startOf('day'));
   const [dueTime, setDueTime] = useState("");
   const [isAllDay, setIsAllDay] = useState(false);
   const [recurrenceFrequency, setRecurrenceFrequency] = useState("");
-  const [unformattedMarillacBucks, setUnformattedMarillacBucks] = useState('');
-  const [formattedMarillacBucks, setFormattedMarillacBucks] = useState('$');
+  const [marillacBucks, setMarillacBucks] = useState('$ ');
 
   const [submitPressed, setSubmitPressed] = useState(false);
 
   const handleSubmit = () => {
     setSubmitPressed(true);
-    if (!title || !location || !dueDate || !dueTime || !formattedMarillacBucks) {
+    if (!title || !location || !dueDate || !dueTime || !marillacBucks) {
       // TODO: Add error handling
     }
     // TODO: API call to add task
@@ -190,25 +280,21 @@ const TaskModal = ({ isOpen, setIsOpen }: Props): React.ReactElement => {
   const resetFormState = () => {
     setTitle("");
     setLocation("");
-    setDueDate("");
+    setDueDate(dayjs().startOf('day'));
     setDueTime("");
     setIsAllDay(false);
     setRecurrenceFrequency("");
-    setUnformattedMarillacBucks("");
-    setFormattedMarillacBucks("$");
+    setMarillacBucks("$");
 
     setSubmitPressed(false);
   };
 
   const handleMoneyInput = () => {
-    console.log("Marillac bucks field blurred. unformattedMarillacBucks: ", unformattedMarillacBucks);
-    let inputValue = unformattedMarillacBucks.replace(/[^0-9.]/g, ''); // Remove non-numeric and non-period characters
+    const inputValue = marillacBucks.replace(/[^0-9.]/g, ''); // Remove non-numeric and non-period characters
     
     if (inputValue) {
-      let numberValue = parseFloat(inputValue).toFixed(2);
-      console.log("rounded numberValue", numberValue);
-      setFormattedMarillacBucks(`$${numberValue}`);
-      console.log("formattedMarillacBucks:", formattedMarillacBucks);
+      const numberValue = parseFloat(inputValue).toFixed(2);
+      setMarillacBucks(`$${numberValue}`);
     }
   };
   
@@ -229,16 +315,18 @@ const TaskModal = ({ isOpen, setIsOpen }: Props): React.ReactElement => {
           submitPressed={submitPressed}
           required
         />
-        <FormLabel color="gray.main" fontWeight="700" mb={0}>
+        <FormLabel color="gray.main" fontWeight="700" mb={0} pb={0}>
           Location
         </FormLabel>
-        <FormControl isRequired mt={0}>
+        <FormControl isRequired mt={0} pt={0}>
           <Select
             variant="primary"
             value={location}
             onChange={(e) => setLocation(e.target.value)}
             border="solid"
+            borderWidth="2px"
             borderColor="gray.300"
+            height="34px"
           >
             <option value="kitchen">Kitchen</option>
             <option value="livingRoom">Living Room</option>
@@ -258,9 +346,9 @@ const TaskModal = ({ isOpen, setIsOpen }: Props): React.ReactElement => {
         />
         <FormField
           label="Marillac Bucks"
-          value={formattedMarillacBucks}
+          value={marillacBucks}
           type="text"
-          onChange={(e) => setUnformattedMarillacBucks(e.target.value)}
+          onChange={(e) => setMarillacBucks(e.target.value)}
           onBlur={handleMoneyInput}
           submitPressed={submitPressed}
           required
