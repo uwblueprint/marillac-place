29f128774131bec1a0ec3a742ca80f8d0de6794e
Jesse Huang <87463074+jeessh@users.noreply.github.com>
2024-09-21 02:41:19 -0400

    fixed announcements page filter/searching

--- frontend/src/components/pages/announcements/AnnouncementsGroups.tsx
+++ frontend/src/components/pages/announcements/AnnouncementsGroups.tsx
@@ -14,6 +14,14 @@ import {
   TabPanel,
   IconButton,
   Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Tag,
+  TagLabel,
+  TagCloseButton,
+  HStack,
 } from "@chakra-ui/react";
 import { Search } from "@mui/icons-material";
 import EditNoteIcon from "@mui/icons-material/EditNote";
@@ -126,6 +134,8 @@ const GroupList: React.FC<{
   setAddingNewRoom,
   selectedRooms,
 }) => {
+  const [searchRooms, setSearchRooms] = useState<number[]>([]);
+  const [allRooms, setAllRooms] = useState([1, 2, 3, 4, 5, 6]);
   const [processedAnnouncements, setProcessedAnnouncements] =
     useState<ProcessedGroupAnnouncements>();
   useEffect(() => {
@@ -169,21 +179,42 @@ const GroupList: React.FC<{
           <></>
         ),
       ].concat(
-        Object.keys(announcementsGroup).map((roomKey) => (
-          <GroupTab
-            key={roomKey}
-            roomKey={roomKey}
-            firstAnnouncement={
-              roomKey === "" ? null : announcementsGroup[roomKey][0]
-            }
-            setSelectedGroup={setSelectedGroup}
-            isDraft={false}
-          />
-        )),
+        Object.keys(announcementsGroup)
+          .filter(
+            (roomKey) =>
+              !searchRooms ||
+              searchRooms.length === 0 ||
+              searchRooms.some((room) =>
+                roomKey.split(",").map(Number).includes(room),
+              ),
+          )
+          .map((roomKey) => (
+            <GroupTab
+              key={roomKey}
+              roomKey={roomKey}
+              isDraft={false}
+              firstAnnouncement={announcementsGroup[roomKey][0]}
+              setSelectedGroup={setSelectedGroup}
+            />
+          )),
       )
     );
   };
 
+  const deleteSearchRoom = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    roomId: number,
+  ) => {
+    if (searchRooms.includes(roomId)) {
+      setSearchRooms(searchRooms.filter((room) => room !== roomId));
+    }
+  };
+
+  const addRoomToSearch = (roomId: number) => {
+    if (!searchRooms.includes(roomId)) {
+      setSearchRooms([...searchRooms, roomId]);
+    }
+  };
   const addRoom = () => {
     setAddingNewRoom(true);
   };
@@ -199,12 +230,56 @@ const GroupList: React.FC<{
           mb={4}
           bg="purple.50"
         >
-          <InputGroup w="80%" bg="white" ml={5}>
-            <InputLeftElement pointerEvents="none">
+          <Box
+            width="100%"
+            marginRight="1.25rem"
+            marginLeft="1.25rem"
+            position="relative"
+            w="100%"
+            bg="white"
+            h="2.5rem"
+            borderRadius="0.375rem"
+            border="1px solid"
+            borderColor="inherit"
+            _hover={{ borderColor: "#C5C8D8" }}
+            _focusVisible={{
+              borderColor: "477FC8",
+              boxShadow: "0 0 0 1px #3182ce",
+            }}
+          >
+            <Menu>
+              <MenuButton width="100%" position="absolute" height="100%" />
+              <MenuList maxH="40vh" overflow="auto" position="absolute">
+                {allRooms
+                  .filter((room) => !searchRooms.includes(room))
+                  .map((room) => (
+                    <MenuItem onClick={() => addRoomToSearch(room)} key={room}>
+                      Room {room}
+                    </MenuItem>
+                  ))}
+              </MenuList>
+            </Menu>
+
+            <HStack spacing={2} height="100%" paddingLeft="8px">
               <Icon as={Search} color="gray.300" />
-            </InputLeftElement>
-            <Input placeholder="Search" />
-          </InputGroup>
+              {searchRooms.map((room) => (
+                <Tag
+                  key={room}
+                  variant="solid"
+                  height="30px"
+                  color="#57469D"
+                  border="1px solid #57469D"
+                  backgroundColor="#F9F7FF"
+                >
+                  <TagLabel textAlign="center"> Room {room} </TagLabel>
+                  <TagCloseButton
+                    onClick={(e) => deleteSearchRoom(e, room)}
+                    color="#57469D"
+                  />
+                </Tag>
+              ))}
+            </HStack>
+          </Box>
           <IconButton
             icon={<EditNoteIcon />}
             aria-label="Edit"
