92f7f2e8a1ee65a43573cc89f0b59e2321b65222
KathleenX7 <kxiong1210@gmail.com>
2024-10-05 11:15:27 -0400

    Resident Mutations

--- frontend/src/components/pages/residents/ResidentsPage.tsx
+++ frontend/src/components/pages/residents/ResidentsPage.tsx
@@ -8,6 +8,17 @@ import {
   InputLeftElement,
 } from "@chakra-ui/react";
 import { Add, Search } from "@mui/icons-material";
+import { useMutation } from "@apollo/client";
+import {
+  ADD_RESIDENT,
+  UPDATE_RESIDENT,
+  DELETE_RESIDENT,
+} from "../../../APIClients/Mutations/ResidentsMutations";
+import {
+  UserResponse,
+  UserRequest,
+  UserRequestUpdate,
+} from "../../../APIClients/Types/ResidentsType";
 
 import CommonTable, {
   ColumnInfoTypes,
@@ -40,6 +51,62 @@ const ResidentsPage = (): React.ReactElement => {
   const [residents, setResidents] = useState<TableData[]>([]);
   const [isModalOpen, setIsModalOpen] = useState("none");
   const [residentEditInfo, setEditInfo] = useState<ResidentEditInfo>();
+
+  const [addResident] = useMutation<{ addResident: UserResponse }>(
+    ADD_RESIDENT,
+  );
+
+  const [updateResident] = useMutation<{
+    userId: number;
+    resident: UserResponse;
+  }>(UPDATE_RESIDENT);
+
+  const [deleteResident] = useMutation<{ userId: number }>(DELETE_RESIDENT);
+
+  // const handleAddResident = async () => {
+  //   try {
+  //     const date = new Date();
+  //     const formattedDate = date.toISOString().split("T")[0];
+
+  //     const resident: UserRequest = {
+  //       email: "dasfhsahfsoad@gmail.com",
+  //       password: "qe8e9r789ewr",
+  //       firstName: "Bob",
+  //       lastName: "Bob",
+  //       residentId: 1248120,
+  //       birthDate: formattedDate,
+  //       roomNumber: 3,
+  //       credits: 500,
+  //       dateJoined: formattedDate,
+  //     };
+  //     await addResident({ variables: { resident } });
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  // const handleUpdateResident = async () => {
+  //   try {
+  //     const userId = 5;
+  //     const resident: UserRequestUpdate = {
+  //       lastName: "NEW NAME",
+  //       roomNumber: 3,
+  //       credits: 10,
+  //     };
+  //     await updateResident({ variables: { userId, resident } });
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  // const handleDeleteResident = async () => {
+  //   try {
+  //     const userId = 1;
+  //     await deleteResident({ variables: { userId } });
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
 
   useEffect(() => {
     // TODO: Fetch residents from API
