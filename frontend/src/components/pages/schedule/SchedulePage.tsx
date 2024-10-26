af9320dc2fe1e198db2e2424034f8a2387bbb173
Jesse Huang <87463074+jeessh@users.noreply.github.com>
2024-10-26 01:02:58 -0400

    Edited styles & linted

--- frontend/src/components/pages/schedule/SchedulePage.tsx
+++ frontend/src/components/pages/schedule/SchedulePage.tsx
@@ -1,32 +1,22 @@
 import React from "react";
-import {
-  Flex,
-} from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
-import RoomCard from "../home/HomeRoomCard"
+import RoomCard from "../home/HomeRoomCard";
 import { residentsMockData } from "../../../mocks/residents";
 import { ScheduleCalendar } from "./ScheduleCalendar";
 
-const renderRoomCards = residentsMockData.map(resident => 
+const renderRoomCards = residentsMockData.map((resident) => (
   <RoomCard
     key={resident.residentId}
     room={resident.roomNumber}
     residentId={resident.residentId}
   />
-)
-
+));
 
 const SchedulePage = (): React.ReactElement => {
   return (
     <Flex flexDir="column" flexGrow={1} p="20px">
-      <h1>Schedule Page</h1>
-      <Flex
-        flexWrap="wrap" 
-        justifyContent="flex-start" 
-      >
-        {renderRoomCards}
-      </Flex>
-      <ScheduleCalendar/>
+      <ScheduleCalendar />
     </Flex>
   );
 };
