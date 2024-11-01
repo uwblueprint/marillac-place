dece4426707e15010e94c276f53df4fcc3d8533b
Jesse Huang <87463074+jeessh@users.noreply.github.com>
2024-10-31 21:42:56 -0400

    ran linter

--- frontend/src/APIClients/Mutations/WarningsMutations.ts
+++ frontend/src/APIClients/Mutations/WarningsMutations.ts
@@ -1,38 +1,29 @@
 import { gql } from "@apollo/client";
 
 export const ADD_WARNING = gql`
-    mutation AddWarning {
-        addWarning(
-            warning: {
-                title: null
-                description: null
-                dateIssued: null
-                assigneeId: null
-                assignerId: null
-                relatedTaskId: null
-            }
-        ) {
-            id
-            title
-            description
-            dateIssued
-            assigneeId
-            assignerId
-            relatedTaskId
-        }
+  mutation AddWarning($warning: CreateWarningDTO!) {
+    addWarning(warning: $warning) {
+      id
+      title
+      description
+      dateIssued
+      assigneeId
+      assignerId
+      relatedTaskId
     }
+  }
 `;
 
 export const DELETE_WARNING = gql`
-    mutation DeleteWarning {
-        deleteWarning(id: null) {
-            id
-            title
-            description
-            dateIssued
-            assigneeId
-            assignerId
-            relatedTaskId
-        }
+  mutation DeleteWarning($warningId: ID!) {
+    deleteWarning(id: $warningId) {
+      id
+      title
+      description
+      dateIssued
+      assigneeId
+      assignerId
+      relatedTaskId
     }
+  }
 `;
--- frontend/src/APIClients/Types/WarningsType.ts
+++ frontend/src/APIClients/Types/WarningsType.ts
@@ -1,18 +1,18 @@
 export type WarningDTO = {
-    id: number;
-    title: string;
-    description: string;
-    dateIssued: Date;
-    assigneeId: number;
-    assignerId: number;
-    relatedTaskId: number;
-  }
+  id: number;
+  title: string;
+  description: string;
+  dateIssued: Date;
+  assigneeId: number;
+  assignerId: number;
+  relatedTaskId: number;
+};
 
 export type CreateWarningDTO = {
-    title: string;
-    description: string;
-    dateIssued: Date;
-    assigneeId: number;
-    assignerId: number;
-    relatedTaskId: number;
-  }
+  title: string;
+  description: string;
+  dateIssued: Date;
+  assigneeId: number;
+  assignerId: number;
+  relatedTaskId: number;
+};
