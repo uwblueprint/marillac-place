bf93040e7df50bb1cc31e635099cb51cbabdf36d
applepie7864 <anniewenqingguo@gmail.com>
2025-02-03 00:41:04 -0500

    moved code out of index into app

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,60 +1,16 @@
-import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import { createUploadLink } from "apollo-upload-client";
-import { setContext } from "@apollo/client/link/context";
-import { jwtDecode } from "jwt-decode";
-
-import {
-  getLocalStorageObjProperty,
-  setLocalStorageObjProperty,
-} from "./utils/LocalStorageUtils";
 
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-const REFRESH_MUTATION = `
-  mutation Index_Refresh {
-    refresh
-  }
-`;
-
-const link = createUploadLink({
-  uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`,
-  credentials: "include",
-});
-
-const authLink = setContext(async (_, { headers }) => {
-  let token = null;
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
-const apolloClient = new ApolloClient({
-  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  link: authLink.concat(link as any),
-  cache: new InMemoryCache(),
-});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
-    <ApolloProvider client={apolloClient}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
