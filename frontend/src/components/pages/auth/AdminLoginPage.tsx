a263bee07deffe443243a4f954bf60c315da52dd
Navraj Bal <balnavsingh@gmail.com>
2025-03-01 12:10:05 -0500

    new ui implementation

--- frontend/src/components/pages/auth/AdminLoginPage.tsx
+++ frontend/src/components/pages/auth/AdminLoginPage.tsx
@@ -12,7 +12,7 @@ import {
   FormErrorMessage,
 } from "@chakra-ui/react";
 
-import { ReactComponent as Logo } from "../../../assets/marillacPlaceLogo.svg";
+import logo from "../../../assets/Marillac-Place-Logo.webp";
 
 const LOGIN = gql`
   mutation Login($password: String!, $role: UserType!) {
@@ -56,83 +56,82 @@ const LoginPage = (): React.ReactElement => {
     <Flex
       w="100vw"
       h="100vh"
-      flexDir="column"
-      alignContent="center"
+      alignItems="center"
       justifyContent="center"
+      bg="white"
     >
-      <Flex position="absolute" top="10px" left="10px">
-        <Logo />
-      </Flex>
-
       <Flex
-        maxW="514px"
-        maxH="480px"
-        mx="auto"
-        p="48px"
+        w="67%"
+        maxW="1200px"
+        h="55%"
+        bg="#E3ECEB"
         borderRadius="8px"
         boxShadow="lg"
-        flexDir="column"
+        flexDir="row"
+        alignItems="center"
+        justifyContent="space-around"
+        p="20px"
       >
-        <Text fontSize="38px" fontWeight="bold" mb="8px">
-          Sign In
-        </Text>
-        <Text fontSize="18px" mb="30px">
-          Please enter your login information
-        </Text>
+        <Flex w="50%" justifyContent="center">
+          <img src={logo} alt="Marillac Place Logo" width="265px" />
+        </Flex>
 
-        <FormControl isInvalid={!!error} alignItems="center">
-          <Select
-            id="role"
-            value={role}
-            onChange={(e) => setRole(e.target.value)}
-            placeholder="Roles"
-            h="49px"
-            borderColor="black"
-            mb="24px"
-            fontSize="18px"
-            color="black"
-          >
-            <option value="" disabled selected hidden style={{ color: "gray" }}>
-              Role
-            </option>
-            <option value="admin_staff">Administrative Staff</option>
-            <option value="release_staff">Release Staff</option>
-          </Select>
-          <Input
-            id="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            h="49px"
-            borderColor="black"
-            fontSize="18px"
-          />
-
-          <FormErrorMessage fontSize="18px">{error}</FormErrorMessage>
-          <Text
-            cursor="pointer"
-            fontWeight="bold"
-            fontSize="18px"
-            textDecoration="underline"
-            float="right"
-            mt="8px"
-          >
-            Forgot Password?
+        <Flex
+          w="50%"
+          maxW="400px"
+          // h="65%"
+          p="40px"
+          borderRadius="8px"
+          boxShadow="md"
+          bg="white"
+          flexDir="column"
+        >
+          <Text fontSize="32px" fontWeight="bold" mb="4">
+            Sign in
           </Text>
-          <Button
-            variant="primary"
-            mt="10px"
-            pt="15px"
-            w="100%"
-            h="60px"
-            borderRadius="48px"
-            onClick={handleSubmit}
-            isLoading={loading}
-          >
-            <Text fontSize="24px">Sign In</Text>
-          </Button>
-        </FormControl>
+          <Text fontSize="16px" mb="6">
+            Please enter your login information.
+          </Text>
+
+          <FormControl isInvalid={!!error}>
+            <Select
+              id="role"
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+              placeholder="Role"
+              mb="4"
+              h="48px"
+              borderColor="gray.300"
+            >
+              <option value="admin_staff">Administrative Staff</option>
+              <option value="release_staff">Release Staff</option>
+            </Select>
+            <Input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              mb="4"
+              h="48px"
+              borderColor="gray.300"
+            />
+
+            <FormErrorMessage>{error}</FormErrorMessage>
+            <Button
+              variant="solid"
+              colorScheme="orange"
+              mt="4"
+              w="100%"
+              h="50px"
+              borderRadius="52px"
+              onClick={handleSubmit}
+              isLoading={loading}
+            >
+              Sign in
+            </Button>
+          </FormControl>
+        </Flex>
       </Flex>
     </Flex>
   );
