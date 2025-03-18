b6011eb672f9432d4845687a25676ba95e5a0b4f
applepie7864 <anniewenqingguo@gmail.com>
2025-03-17 20:18:06 -0400

    new add participant flow

--- frontend/src/components/pages/participants/EmptyParticipantCard.tsx
+++ frontend/src/components/pages/participants/EmptyParticipantCard.tsx
@@ -1,36 +1,47 @@
 import { Button, Flex, Text } from '@chakra-ui/react';
-import React from 'react'
+import React, { useState } from 'react'
+import AddParticipantCard from './AddParticipantCard';
 
 type EmptyParticipantCardProps = {
     roomNumber: string;
 }
 
 const EmptyParticipantCard = ({roomNumber}: EmptyParticipantCardProps) => {
-  return (
-    <Flex 
-        w="180px"
-        h="130px"
-        border="2px solid"
-        borderColor="#E2E8F0"
-        borderRadius="5px"
-        flexDir="column"
-        justifyContent="top"
-        alignItems="center"
-    >
-        <Flex w="full" justifyContent="center" p="5px" bg="#E3ECEB" fontSize="small" fontWeight="700">Room {roomNumber}</Flex>
-        <Flex
-            w="full"
-            h="full"
+    const [addParticipant, setAddParticipant] = useState(false);
+    return (
+        <Flex 
+            w="180px"
+            h="130px"
+            border="2px solid"
+            borderColor="#E2E8F0"
+            borderRadius="5px"
             flexDir="column"
+            justifyContent="top"
             alignItems="center"
-            justifyContent="center"
-            gap="5px"
         >
-            <Flex fontSize="xs">The room is empty.</Flex>
-            <Button size="xs" bg="orange.500" color="white" mt="3px">Add Participant</Button>
+            <Flex w="full" justifyContent="center" p="5px" bg="#E3ECEB" fontSize="small" fontWeight="700">Room {roomNumber}</Flex>
+            <Flex
+                w="full"
+                h="full"
+                flexDir="column"
+                alignItems="center"
+                justifyContent="center"
+                gap="5px"
+            >
+                <Flex fontSize="xs">The room is empty.</Flex>
+                <Button 
+                    size="xs" 
+                    bg="orange.500" 
+                    color="white" 
+                    mt="3px"
+                    onClick={() => setAddParticipant(true)}
+                >
+                    Add Participant
+                </Button>
+            </Flex>
+            {addParticipant && <AddParticipantCard roomNumber={roomNumber} close={() => setAddParticipant(false)} />}
         </Flex>
-    </Flex>
-  )
+    )
 }
 
 export default EmptyParticipantCard
