5a380b52d8ae8a7a1a5e20654629d42b8e04b5de
applepie7864 <anniewenqingguo@gmail.com>
2025-03-29 12:42:26 -0400

    save

--- frontend/src/components/pages/home/NoteSection.tsx
+++ frontend/src/components/pages/home/NoteSection.tsx
@@ -1,24 +1,60 @@
-import { Flex, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Flex, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import SendSvg from '../../../assets/svg/SendSvg';
+import FormInputField from '../../common/FormInputField';
 
 const NoteSection = () => {
+    const [note, setNote] = useState("");
+
     return (
         <Flex
             margin="10px"
             paddingY="10px"
             paddingX="20px"
-            w="250px"
+            w="300px"
             border='2px' 
             borderColor='gray.200'
             borderRadius="md"
         >
-            <Text
-                fontSize="lg"
-                fontWeight="600"
-                color="#0C727E"
+            <Flex
+                w="100%"
+                h="25px"
+                flexDir="row"
+                justifyContent="space-between"
+                alignItems="center"
             >
-                Notes
-            </Text>
+                <Text
+                    fontSize="lg"
+                    fontWeight="600"
+                    color="#0C727E"
+                    mb="0px"
+                >
+                    Admin Notes
+                </Text>
+                <Text
+                    fontSize="xs"
+                    fontWeight="400"
+                    color="gray.400"
+                    mb="0px"
+                >
+                    Expires in 48h
+                </Text>
+            </Flex>
+            <FormInputField 
+                label=""
+                value={note}
+                type="text"
+                onChange={(e) => setNote(e.target.value)}
+                
+            />
+
+            {/* label: string;
+              placeholder?: string;
+              value: string | number | undefined;
+              type: "text" | "password" | "date" | "number";
+              onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+              required?: boolean;
+              leftElement?: string; */}
         </Flex>
     )
 }
