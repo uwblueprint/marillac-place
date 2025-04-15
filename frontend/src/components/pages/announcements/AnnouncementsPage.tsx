59d63cf4a18b213fab68ecaedd220e98c3e2f163
karthikbselva <karthikbselva@gmail.com>
2025-04-14 20:00:38 -0400

    Adjusted UI and added announcements button

--- frontend/src/components/pages/announcements/AnnouncementsPage.tsx
+++ frontend/src/components/pages/announcements/AnnouncementsPage.tsx
@@ -29,24 +29,37 @@ const AnnouncementsPage = (): React.ReactElement => {
   return (
     <Flex>
       <SideBar />
-      <Flex flexDir="column" flex={1} ml={20} mr={5} mt={10}>
-        {/* Heading and Expiry Text */}
-        <Flex alignItems="center" mb={4}>
-          <Heading size="lg" mr={3}>Announcements</Heading>
-          <Text fontSize="sm" color="gray.600">Expires in 48 hr</Text>
+      <Flex flexDir="column" flex={1} ml={20} mr={5} mt={20}>
+        {/* Header */}
+        <Flex alignItems="center" justifyContent="space-between" mb={4}>
+          <Flex alignItems="flex-end">
+            <Heading size="lg" mr={3}>Announcements</Heading>
+            <Flex mb={1.5} ml={2}>
+              <h6 color="lightgray.600">Expires in 48h</h6>
+            </Flex>
+          </Flex>
+          <Flex mt={3} mr={20}>
+          <Button colorScheme="orange" size="sm">
+            + Create Announcement
+          </Button>
+          </Flex>
         </Flex>
 
-        {/* Filter Buttons Row */}
-        <Flex alignItems="center" wrap="wrap" mb={4}>
-          <Text mr={4} mb={2} whiteSpace="nowrap">Filter:</Text>
-          <Flex wrap="wrap" alignItems="center" flex="1">
+        {/* Filters */}
+        <Flex alignItems="center" wrap="wrap" mb={4} ml={1}>
+          <Text mr={4} mb={2} whiteSpace="nowrap" color="black.1000">Filters:</Text>
+          <Flex wrap="wrap" alignItems="center">
             {selectedButtons.map((isSelected, index) => (
               <Button
                 key={index}
                 onClick={() => handleButtonClick(index)}
                 variant={isSelected ? "solid" : "outline"}
                 size="sm"
-                mr={2}
+                borderColor="teal.500"
+                color={isSelected ? "white" : "teal.600"}
+                bg={isSelected ? "teal.500" : "transparent"}
+                _hover={{ bg: isSelected ? "teal.600" : "teal.50" }}
+                mr={3}
                 mb={2}
               >
                 Room {index + 1}
@@ -55,8 +68,10 @@ const AnnouncementsPage = (): React.ReactElement => {
             <Button
               variant="link"
               textDecoration="underline"
+              color="gray.600"
               onClick={handleSelectAll}
-              mr={4}
+              size="sm"
+              mr={3}
               mb={2}
             >
               Select All
@@ -64,15 +79,15 @@ const AnnouncementsPage = (): React.ReactElement => {
             <Button
               variant="link"
               textDecoration="underline"
+              color="gray.600"
               onClick={handleDeselectAll}
+              size="sm"
               mb={2}
             >
               Deselect All
             </Button>
           </Flex>
         </Flex>
-
-        {/* Additional page content can go here */}
       </Flex>
     </Flex>
   );
