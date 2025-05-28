a45c7a9423204d3ae31ba1a3f6e0bde77ae43f60
yan <yanyam.xue@gmail.com>
2025-05-27 21:22:12 -0400

    integrated expireAdminAnnoucements function

---
+++ backend/crons/scripts/expireAdminAnnoucements.ts
@@ -0,0 +1,20 @@
+import prisma from "../../prisma";
+
+async function expireAdminAnnouncements(): Promise<boolean> {
+  const limit = new Date(Date.now() - 48 * 60 * 60 * 1000).toLocaleDateString("en-ca")
+  try {
+    await prisma.announcement.deleteMany({
+      where: {
+        creation_date: {
+          lt: limit,
+        },
+      },
+    });
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+}
+
+export default expireAdminNotes;
