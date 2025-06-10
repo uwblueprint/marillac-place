fbcc5b0d8e3d0518fdae463d546740b33089d531
Gavin Xue <gavinxue050705@gmail.com>
2025-06-10 19:38:59 -0400

    fixed spacing and added overflow

--- frontend/src/pages/admin/home/elements/RoomsOverview.tsx
+++ frontend/src/pages/admin/home/elements/RoomsOverview.tsx
@@ -21,55 +21,71 @@ export default function RoomsOverview() {
       <Text textStyle="web.h3" color="primary.700" marginBottom="15px">
         Rooms
       </Text>
-      <Grid w="100%" templateColumns="repeat(5, 1fr)" gap="15px">
-        {ROOM_NUMBERS.map((num) => (
-          <Flex
-            key={num}
-            minHeight="150px"
-            border="1px solid"
-            borderColor="neutral.300"
-            borderRadius="8px"
-            flexDir="column"
-            justifyContent="space-between"
-            alignItems="center"
-            // gap="4px"
-            // padding="10px"
-          >
-            <Text
-              textStyle="web.s1"
-              bg="primary.100"
-              width="100%"
-              textAlign="center"
-              padding="8px"
-              borderBottom="1px solid"
+      <Flex
+        overflow="scroll"
+        justifyContent="center"
+        sx={{
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+        }}
+      >
+        <Grid w="100%" templateColumns="repeat(5, 1fr)" gap="15px">
+          {ROOM_NUMBERS.map((num) => (
+            <Flex
+              key={num}
+              minWidth="100px"
+              minHeight="120px"
+              border="1px solid"
               borderColor="neutral.300"
+              borderRadius="8px"
+              flexDir="column"
+              justifyContent="space-between"
+              alignItems="center"
+              paddingBottom="10px"
             >
-              Room #{num}
-            </Text>
-
-            <Text textStyle="web.b3" textAlign="center">
-              ID Number:{" "}
-              <Text as="span" textStyle="web.s1">
-                #123
+              <Text
+                textStyle="web.s1"
+                bg="primary.100"
+                width="100%"
+                textAlign="center"
+                padding="8px"
+                borderBottom="1px solid"
+                borderColor="neutral.300"
+              >
+                Room #{num}
               </Text>
-            </Text>
-            <Text textStyle="web.b3" textAlign="center">
-              # Assigned Tasks
-            </Text>
-            <Link
-              textDecoration="underline"
-              textAlign="center"
-              textStyle="web.b3"
-              href="/admin/schedule"
-            >
-              View Schedule
-            </Link>
-            {/* <Text textStyle="web.b3" textAlign="center">
+
+              <Text textStyle="web.b3" textAlign="center">
+                ID Number:{" "}
+                <Text as="span" textStyle="web.s1">
+                  #123
+                </Text>
+              </Text>
+              <Text textStyle="web.b3" textAlign="center">
+                # Assigned Tasks
+              </Text>
+              <Link
+                href="/admin/schedule"
+                textDecoration="underline"
+                textAlign="center"
+                textStyle="web.b3"
+                fontFamily="Nunito"
+                fontWeight={600}
+                color="black"
+                _hover={{
+                  textDecoration: "none",
+                }}
+              >
+                View Schedule
+              </Link>
+              {/* <Text textStyle="web.b3" textAlign="center">
               Room is available.
             </Text> */}
-          </Flex>
-        ))}
-      </Grid>
+            </Flex>
+          ))}
+        </Grid>
+      </Flex>
     </Flex>
   );
 }
