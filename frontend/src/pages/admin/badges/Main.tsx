1f99d9d4c4a5f6cfd37412c9456ca075683706f4
Gavin Xue <gavinxue050705@gmail.com>
2025-06-15 20:38:29 -0400

    started table ui

--- frontend/src/pages/admin/badges/Main.tsx
+++ frontend/src/pages/admin/badges/Main.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import {Flex, Text} from '@chakra-ui/react'
+import CustomBadgesTable from "./elements/CustomBadgesTable";
 
 export default function AdminBadgesPage() {
   return (
-    <div>badges page</div>
+    <Flex w="100%" flexDir="column" minHeight="fit-content">
+      <Text textStyle="web.h2" color="primary.700" mb="10px">Current Participants</Text>
+      <CustomBadgesTable/>
+    </Flex>
   )
 }
