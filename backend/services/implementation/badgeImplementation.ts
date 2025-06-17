f7a35570d51a41f6a95848e4bf76f40c1968a834
Gavin Xue <gavinxue050705@gmail.com>
2025-06-17 19:49:08 -0400

    gql endpoint for get custom badges

--- backend/services/implementation/badgeImplementation.ts
+++ backend/services/implementation/badgeImplementation.ts
@@ -1,30 +1,60 @@
-import { EarnedBadge, BadgeType } from "@prisma/client";
+import { EarnedBadge, BadgeType, Badge } from "@prisma/client";
 import IBadgeService from "../interface/badgeInterface";
 import prisma from "../../prisma";
 
 class BadgeService implements IBadgeService {
-  async editCustomBadge (
+  async getCustomBadges(): Promise<Badge[]> {
+    try {
+      const customBadges = await prisma.badge.findMany({
+        where: {
+          badge_type: "CUSTOM",
+        },
+        include: {
+          badge_level: true,
+        },
+      });
+      return customBadges;
+    } catch (err) {
+      if (err instanceof Error) {
+        throw new Error(err.message || "Failed to get custom badges.");
+      }
+      throw new Error("Failed to get custom badges.");
+    }
+  }
+
+  async editCustomBadge(
     custom_badge_id: number,
     new_custom_badge_name?: string,
     new_custom_badge_description?: string
   ): Promise<boolean> {
     try {
-      if (new_custom_badge_name == undefined && new_custom_badge_description == undefined) throw new Error("No edits provided");
+      if (
+        new_custom_badge_name == undefined &&
+        new_custom_badge_description == undefined
+      )
+        throw new Error("No edits provided");
       await prisma.badge.update({
-        where: { badge_id: custom_badge_id, badge_type: 'CUSTOM' },
+        where: { badge_id: custom_badge_id, badge_type: "CUSTOM" },
         data: {
-          ...(new_custom_badge_name !== undefined && { name: new_custom_badge_name }),
-          ...(new_custom_badge_description !== undefined && { description: new_custom_badge_description })
+          ...(new_custom_badge_name !== undefined && {
+            name: new_custom_badge_name,
+          }),
+          ...(new_custom_badge_description !== undefined && {
+            description: new_custom_badge_description,
+          }),
         },
       });
       return true;
     } catch (err) {
       // @ts-ignore
-      throw new Error(err.message ||"Something went wrong");
+      throw new Error(err.message || "Something went wrong");
     }
   }
 
-  async assignCustomBadge(badge_id: number, participant_id: number): Promise<EarnedBadge> {
+  async assignCustomBadge(
+    badge_id: number,
+    participant_id: number
+  ): Promise<EarnedBadge> {
     // First get the badge details to copy information
     const badge = await prisma.badge.findUnique({
       where: { badge_id },
--- backend/services/interface/badgeInterface.ts
+++ backend/services/interface/badgeInterface.ts
@@ -1,12 +1,16 @@
 import { Badge, EarnedBadge } from "@prisma/client";
 
 interface IBadgeService {
+  getCustomBadges(): Promise<Badge[]>;
   editCustomBadge(
     custom_badge_id: number,
     new_custom_badge_name?: string,
     new_custom_badge_description?: string
   ): Promise<boolean>;
-  assignCustomBadge(badge_id: number, participant_id: number): Promise<EarnedBadge>;
+  assignCustomBadge(
+    badge_id: number,
+    participant_id: number
+  ): Promise<EarnedBadge>;
 }
 
 export default IBadgeService;
