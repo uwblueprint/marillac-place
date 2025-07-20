d6400e5a628047b92c4a4eb735d0ed624625e165
elsieliii <yli.elsie@gmail.com>
2025-07-20 18:45:04 -0400

    critical is important

--- backend/services/implementation/announcementImplementation.ts
+++ backend/services/implementation/announcementImplementation.ts
@@ -156,7 +156,7 @@ class AnnouncementService implements IAnnouncementService {
     } else if (filter === "PINNED") {
       where.pinned = true;
     } else if (filter === "IMPORTANT") {
-      where.announcement = { priority: "HIGH" };
+      where.announcement = { priority: "CRITICAL" };
     }
 
     return prisma.userAnnouncement.findMany({
