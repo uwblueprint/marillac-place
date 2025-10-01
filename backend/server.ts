7fb0be032d8291e01796b09a63a51140d41bb0d6
VictorChan880 <victorchan870@mgmail.com>
2025-09-30 20:42:06 -0400

    updated import for server.js

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import getGraphQLSchema from "./utils/getGraphQLSchema";
-require("./crons/index.ts");
+import "./crons";
 
 const app = express();
 
