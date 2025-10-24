0a1fdbc4541e47e09a4723af0334cae973b6dee7
Calista Besseling <calista.besseling@gmail.com>
2025-10-23 21:41:05 -0400

    Added set goal popup

--- frontend/src/participant/pages/progress/Main.tsx
+++ frontend/src/participant/pages/progress/Main.tsx
@@ -1,77 +1,59 @@
-import {
- Button,
- Flex,
- Input,
- InputGroup,
- InputLeftElement,
- Text,
- Switch,
- useDisclosure
-} from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import React, { useState } from 'react';
+import { Button, Flex, Text } from '@chakra-ui/react';
 import ParticipantPageHeader from '../../common/PageHeader';
-import CreateProgressModal from "./components/CreateProgressModal";
-import OrangeButton from "../../../admin/common/buttons/OrangeButton";
-
-
 import { EditGoal } from './elements/EditGoal';
+import { SetGoal } from './elements/SetGoal';
+
 import WeeklyEarningsChart from "./elements/EarningsWidget";
 
 export default function ParticipantsProgressPage() {
-const [create, setCreate] = useState(false);
-const [editGoal, setEditGoal] = useState(false);
+  const [editGoal, setEditGoal] = useState(false);
+  const [setGoal, setSetGoal] = useState(false);
   const [error, setError] = useState<string | undefined>(undefined);
   const sampleWeeklyEarnings = [10, 15, 8, 20, 12, 18, 35];
   
   const handleClose = () => {
+    setSetGoal(false);
     setEditGoal(false);
-  }
+  };
 
- return (
-   <>
-     <ParticipantPageHeader currentPage="Progress" />
-     <Flex
-       width="90%"
-       alignItems="flex-start"
-       alignSelf="center"
-       justifyContent="space-between"
-       backgroundColor="white"
-       gap="15px"
-       paddingX="30px"
-       paddingY="20px"
-       border="1px"
-       borderColor="#C5C8D8"
-       borderRadius="8px"
-       marginTop="20px"
-     >
-       <Flex flexDirection="column" justifyContent="flex-start">
-         <Text
-           textStyle="web.c3" color="text.light.primary" fontWeight="800"
-         >
-           Marillac Bucks Goal
-         </Text> 
-         <Text
-           textStyle="web.c3"
-           color="text.light.primary"
-           marginTop="9px"
-         >
-           Set a new goal to track your progress!
-         </Text> 
-       </Flex>
+ 
+  
 
-
-       <Flex flexDirection="column" justifyContent="flex-start">
-         <OrangeButton
-                 text="Set Goal"
-                 action={() => setCreate(true)}
-                 is_active={create}
+  return (
+    <>
+      <ParticipantPageHeader currentPage="Progress" />
+      <Flex 
+        width="90%"
+        alignItems="flex-start"
+        alignSelf="center"
+        justifyContent="space-between"
+        backgroundColor="white"
+        gap="15px"
+        paddingX="30px"
+        paddingY="20px"
+        border="1px"
+        borderColor="#C5C8D8"
+        borderRadius="8px"
+        marginTop="20px">
+        <Flex flexDirection="row" justifyContent="space-between" width="100%">
+          <Text
+            textStyle="web.c1" color="text.light.primary" fontWeight="800"
+          >
+            Marillac Bucks Goal
+          </Text> 
+        
+          <Text borderBottom="1px" textStyle="web.c1" onClick={() => setSetGoal(true)}>Set Goal</Text>
+        </Flex>
+      </Flex>
+      { setGoal &&
+        <SetGoal 
+          handleClose={handleClose} 
         />
-       </Flex>
-       {create && <CreateProgressModal isOpen={create} onClose={() => setCreate(false)} />}
-     </Flex>
-     <div style={{ padding: "10px 20px" }}>
+      }
+      <div style={{ padding: "10px 20px" }}>
         <WeeklyEarningsChart weeklyEarnings={sampleWeeklyEarnings} />
       </div>
-   </>
- )
-};
+    </>
+  );
+}
