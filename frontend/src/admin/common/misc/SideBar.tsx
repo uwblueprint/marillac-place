8252d4a6058ad545ec314dfe3c50c3ee7ce8eda1
Judy Yang <judyyang12404@gmail.com>
2025-10-25 22:48:48 -0400

    reports tab

--- frontend/src/admin/common/misc/SideBar.tsx
+++ frontend/src/admin/common/misc/SideBar.tsx
@@ -5,12 +5,7 @@ import {
   TabList,
   Tab,
   Box,
-  Button,
   Flex,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalBody,
   Text,
 } from "@chakra-ui/react";
 import * as ROUTES from "../../../constants/routes";
@@ -82,6 +77,7 @@ export default function SideBar() {
     { label: "Participants", route: ROUTES.ADMIN_PARTICIPANTS_PAGE },
     { label: "Task Library", route: ROUTES.ADMIN_TASKS_PAGE },
     { label: "Badge Library", route: ROUTES.ADMIN_BADGES_PAGE },
+    { label: "Reports", route: ROUTES.ADMIN_REPORTS_PAGE },
   ];
 
   const currentPage = pages.findIndex(
