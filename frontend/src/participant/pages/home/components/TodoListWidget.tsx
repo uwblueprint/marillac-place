a7a1bf3c8f45ae29c70addb849bbfec90593f04a
Navraj Bal <balnavsingh@gmail.com>
2025-10-26 07:10:27 -0400

    todolistwidget format and add interfact for assigned task

--- frontend/src/participant/pages/home/components/TodoListWidget.tsx
+++ frontend/src/participant/pages/home/components/TodoListWidget.tsx
@@ -1,14 +1,6 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import {
-  Box,
-  Text,
-  VStack,
-  Spinner,
-  Alert,
-  AlertIcon,
-  Flex,
-} from "@chakra-ui/react";
+import { Text, Spinner, Alert, AlertIcon, Flex } from "@chakra-ui/react";
 import { useQuery } from "@apollo/client";
 import { ParticipantContext } from "../../../common/ParticipantContext";
 import { GET_ASSIGNED_TASKS_BY_PARTICIPANT_ID_AND_DATE } from "../../../../gql/queries";
@@ -16,42 +8,56 @@ import { PARTICIPANTS_SCHEDULE_PAGE } from "../../../../constants/routes";
 import { formatTimeRange, getToday } from "../../../../utils/formatDateTime";
 import { TaskStatus } from "../../../../admin/pages/schedule/components/ScheduleTypes";
 import Icon from "../../../common/Icon";
-import assigned from "../../../icons/status/assigned.svg"
-import complete from "../../../icons/status/complete.svg"
-import incomplete from "../../../icons/status/incomplete.svg"
-import excused from "../../../icons/status/excused.svg"
-import comment from "../../../icons/misc/comment.svg"
+import assigned from "../../../icons/status/assigned.svg";
+import complete from "../../../icons/status/complete.svg";
+import incomplete from "../../../icons/status/incomplete.svg";
+import excused from "../../../icons/status/excused.svg";
+import comment from "../../../icons/misc/comment.svg";
+
+interface AssignedTask {
+  assigned_task_id: number;
+  task_name: string;
+  task_status: TaskStatus;
+  start_date: string;
+  end_date: string;
+  comment: string;
+}
 
 const TodoListWidget = () => {
-  const participant = useContext(ParticipantContext) as { id: number } | undefined;
+  const participant = useContext(ParticipantContext) as
+    | { id: number }
+    | undefined;
   const participantId = participant?.id;
 
   const navigate = useNavigate();
   const date = getToday();
 
-  const { data, loading, error } = useQuery(GET_ASSIGNED_TASKS_BY_PARTICIPANT_ID_AND_DATE, {
-    variables: { participantId, date },
-    skip: !participantId,
-  });
+  const { data, loading, error } = useQuery(
+    GET_ASSIGNED_TASKS_BY_PARTICIPANT_ID_AND_DATE,
+    {
+      variables: { participantId, date },
+      skip: !participantId,
+    }
+  );
 
-  return  (
+  return (
     <>
       <Flex direction="row" justify="space-between" align="center">
         <Text textStyle="mobile.h2">To-Do List</Text>
-        <Text 
+        <Text
           color="text.light.primary"
           textStyle="mobile.h3"
-          textDecoration="underline" 
+          textDecoration="underline"
           cursor="pointer"
           _hover={{
-            textDecoration: "none"
+            textDecoration: "none",
           }}
           onClick={() => navigate(PARTICIPANTS_SCHEDULE_PAGE)}
         >
           Schedule
         </Text>
       </Flex>
-        
+
       {loading && <Spinner />}
 
       {error && (
@@ -61,37 +67,44 @@ const TodoListWidget = () => {
         </Alert>
       )}
 
-      {!loading && !error && data && data.getAssignedTasksByParticipantIdAndDate.length !== 0 && (
-        data.getAssignedTasksByParticipantIdAndDate.map((assigned_task: any) => {
-          return (
-            <Flex
-              key={assigned_task.assigned_task_id}
-              justify="space-between"
-              align="center"
-            >
-              <Flex gap="8px">
-                { assigned_task.task_status === TaskStatus.ASSIGNED ? (
-                  <Icon icon={assigned} width="20px" height="20px" />
-                ): assigned_task.task_status === TaskStatus.COMPLETE ? (
-                  <Icon icon={complete} width="20px" height="20px" />
-                ): assigned_task.task_status === TaskStatus.INCOMPLETE ? (
-                  <Icon icon={incomplete} width="20px" height="20px" />
-                ): (
-                  <Icon icon={excused} width="20px" height="20px" />
-                )}
-                <Text textStyle="mobile.b1">{assigned_task.task_name}</Text>
-                { assigned_task.comment && (
-                  <Icon icon={comment} width="12px" height="12px" />
-                )}
+      {!loading &&
+        !error &&
+        data &&
+        data.getAssignedTasksByParticipantIdAndDate.length !== 0 &&
+        data.getAssignedTasksByParticipantIdAndDate.map(
+          (assigned_task: AssignedTask) => {
+            return (
+              <Flex
+                key={assigned_task.assigned_task_id}
+                justify="space-between"
+                align="center"
+              >
+                <Flex gap="8px">
+                  {assigned_task.task_status === TaskStatus.ASSIGNED ? (
+                    <Icon icon={assigned} width="20px" height="20px" />
+                  ) : assigned_task.task_status === TaskStatus.COMPLETE ? (
+                    <Icon icon={complete} width="20px" height="20px" />
+                  ) : assigned_task.task_status === TaskStatus.INCOMPLETE ? (
+                    <Icon icon={incomplete} width="20px" height="20px" />
+                  ) : (
+                    <Icon icon={excused} width="20px" height="20px" />
+                  )}
+                  <Text textStyle="mobile.b1">{assigned_task.task_name}</Text>
+                  {assigned_task.comment && (
+                    <Icon icon={comment} width="12px" height="12px" />
+                  )}
+                </Flex>
+
+                <Text color="text.light.secondary" textStyle="mobile.b1">
+                  {formatTimeRange(
+                    assigned_task.start_date,
+                    assigned_task.end_date
+                  )}
+                </Text>
               </Flex>
-              
-              <Text color="text.light.secondary" textStyle="mobile.b1">
-                {formatTimeRange(assigned_task.start_date, assigned_task.end_date)}
-              </Text>
-            </Flex>
-          );
-        })
-      )}
+            );
+          }
+        )}
     </>
   );
 };
