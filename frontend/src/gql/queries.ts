ef83c0d639179452623c3a061fc35aa513e3616d
Judy Yang <judyyang12404@gmail.com>
2025-10-30 20:14:47 -0400

    connected fe to db

--- frontend/src/gql/queries.ts
+++ frontend/src/gql/queries.ts
@@ -347,3 +347,15 @@ export const GET_ASSIGNED_TASKS = gql`
     }
   }
 `;
+
+export const GET_REPORT_RECIPIENTS = gql`
+  query getReportRecipients {
+    getReportRecipients {
+      report_recipient_id
+      email
+      weekly
+      monthly
+      last_report_sent
+    }
+  }
+`;
