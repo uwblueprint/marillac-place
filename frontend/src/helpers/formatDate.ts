d07134b0895fcfd6717a4eab1e1b2c08c7b20b84
applepie7864 <anniewenqingguo@gmail.com>
2025-11-13 19:49:46 -0500

    inline todo comment for navraj

--- frontend/src/helpers/formatDate.ts
+++ frontend/src/helpers/formatDate.ts
@@ -1,4 +1,6 @@
-// TODO:
-// Identify all date formats used in the frontend
+// TODO (navraj):
+// Identify all string date formats used in the frontend
 // For each format, implement a function that converts a Date object to the local timezone and formats it accordingly
+// Additionally, for each React input type that accepts a date or time, implement a function that converts the input (usually React processes it as a formatted string) to a Date object
+// If date / time does not matter, set the date or time to something standard (e.g. midnight, or January 1, 1970)
 export {};
