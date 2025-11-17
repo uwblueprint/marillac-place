cf6ea1953bd3b9b74ebd703eb1bf6c2fd6068588
applepie7864 <anniewenqingguo@gmail.com>
2025-11-17 13:30:43 -0500

    marked tasks

--- backend/utils/badgeUtils.ts
+++ backend/utils/badgeUtils.ts
@@ -67,6 +67,7 @@ export async function updateBadgeLevelProgress(
       where: { name_level_pid: { name, level: badgeLevelProgress.level, pid } },
     });
 
+    // TODO: Process the earning only upon notifying the participant
     const reasonForEarning = `${badgeLevelProgress.level} ${name} badge achieved!`;
     await processEarning(
       pid,
