5c32ba7ff147d0f2691fc650c2a56b72d88a540f
jennifershi255 <jennifershi255@gmail.com>
2025-11-20 21:07:21 -0500

    refactor badges

--- frontend/src/admin/pages/badges/Main.tsx
+++ frontend/src/admin/pages/badges/Main.tsx
@@ -1,104 +1,107 @@
 export {};
-// TODO: Refactor this component
-// import { Flex, Text } from "@chakra-ui/react";
-// import { useQuery } from "@apollo/client";
-// import React, { useEffect, useState } from "react";
-// import CreateCustomBadgeModal from "./components/CreateCustomBadgeModal";
-// import CustomBadgeTable from "./components/CustomBadgeTable";
-// import SystemBadgeTable from "./components/SystemBadgeTable";
-// import { GET_CUSTOM_BADGES, GET_SYSTEM_BADGES } from "../../../gql/queries";
-// import AssignCustomBadgeModal from "./components/AssignCustomBadgeModal";
-// import GreenButton from "../../common/buttons/GreenButton";
-// import OrangeButton from "../../common/buttons/OrangeButton";
-// 
-// export default function AdminBadgesPage() {
-//   const [create, setCreate] = useState(false);
-//   const [assign, setAssign] = useState(false);
-// 
-//   const [customBadges, setCustomBadges] = useState([]);
-//   const [systemBadges, setSystemBadges] = useState([]);
-// 
-//   const {
-//     loading: customBadgesLoading,
-//     error: customBadgesError,
-//     data: customBadgesData,
-//   } = useQuery(GET_CUSTOM_BADGES);
-// 
-//   const {
-//     loading: systemBadgesLoading,
-//     error: systemBadgesError,
-//     data: systemBadgesData,
-//   } = useQuery(GET_SYSTEM_BADGES);
-// 
-//   useEffect(() => {
-//     if (!systemBadgesLoading && !systemBadgesError && systemBadgesData) {
-//       setSystemBadges(systemBadgesData.getSystemBadges);
-//     }
-//   }, [systemBadgesLoading, systemBadgesError, systemBadgesData]);
-// 
-//   useEffect(() => {
-//     if (!customBadgesLoading && !customBadgesError && customBadgesData) {
-//       setCustomBadges(customBadgesData.getCustomBadges);
-//     }
-//   }, [customBadgesLoading, customBadgesError, customBadgesData]);
-// 
-//   return (
-//     <Flex width="100%" height="fit-content" flexDir="column" gap="15px">
-//       <Flex
-//         width="100%"
-//         height="fit-content"
-//         alignItems="center"
-//         justifyContent="space-between"
-//       >
-//         <Flex alignItems="center" gap="15px">
-//           <Text textStyle="web.h2" color="primary.700">
-//             System Badges
-//           </Text>
-//           <Text textStyle="web.b3" color="text.light.secondary" marginTop="7px">
-//             System badges will be granted to participants automatically.
-//           </Text>
-//         </Flex>
-//       </Flex>
-//       <SystemBadgeTable
-//         loading={systemBadgesLoading}
-//         error={systemBadgesError}
-//         badges={systemBadges}
-//       />
-//       <Flex
-//         width="100%"
-//         height="fit-content"
-//         alignItems="center"
-//         justifyContent="space-between"
-//       >
-//         <Flex alignItems="center" gap="15px">
-//           <Text textStyle="web.h2" color="primary.700">
-//             Custom Badges
-//           </Text>
-//           <Text textStyle="web.b3" color="text.light.secondary" marginTop="7px">
-//             You can create new and reward participants custom badges.
-//           </Text>
-//         </Flex>
-//         <Flex alignItems="center" gap="15px">
-//           <GreenButton
-//             text="Assign Custom Badge"
-//             action={() => setAssign(true)}
-//             is_active={assign}
-//           />
-//           <OrangeButton
-//             text="Create New"
-//             action={() => setCreate(true)}
-//             is_active={create}
-//           />
-//         </Flex>
-//       </Flex>
-//       <CustomBadgeTable
-//         loading={customBadgesLoading}
-//         error={customBadgesError}
-//         badges={customBadges}
-//       />
-// 
-//       {create && <CreateCustomBadgeModal onClose={() => setCreate(false)} />}
-//       {assign && <AssignCustomBadgeModal onClose={() => setAssign(false)} />}
-//     </Flex>
-//   );
-// }
+//TODO: Refactor this component
+import { Flex, Text } from "@chakra-ui/react";
+import { useQuery } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+import CreateCustomBadgeModal from "./components/CreateCustomBadgeModal";
+import CustomBadgeTable from "./components/CustomBadgeTable";
+import SystemBadgeTable from "./components/SystemBadgeTable";
+import { GET_CUSTOM_BADGES } from "../../../gql/customBadgeRequests";
+import { GET_SYSTEM_BADGES } from "../../../gql/systemBadgeRequests";
+import resolvers from "../../backend/gql/resolvers";
+import AssignCustomBadgeModal from "./components/AssignCustomBadgeModal";
+import GreenOutlineButton from "../../../ui/buttons/GreenOutlineButton";
+import OrangeButton from "../../../ui/buttons/OrangeButton";
+import { SystemBadge, CustomBadge } from "../../../types/models";
+
+export default function AdminBadgesPage() {
+  const [create, setCreate] = useState(false);
+  const [assign, setAssign] = useState(false);
+
+  const [customBadges, setCustomBadges] = useState<CustomBadge[]>([]);
+  const [systemBadges, setSystemBadges] = useState<SystemBadge[]>([]);
+
+  const {
+    loading: customBadgesLoading,
+    error: customBadgesError,
+    data: customBadgesData,
+  } = useQuery(GET_CUSTOM_BADGES);
+
+  const {
+    loading: systemBadgesLoading,
+    error: systemBadgesError,
+    data: systemBadgesData,
+  } = useQuery(GET_SYSTEM_BADGES);
+
+  useEffect(() => {
+    if (!systemBadgesLoading && !systemBadgesError && systemBadgesData) {
+      setSystemBadges(systemBadgesData.getSystemBadges);
+    }
+  }, [systemBadgesLoading, systemBadgesError, systemBadgesData]);
+
+  useEffect(() => {
+    if (!customBadgesLoading && !customBadgesError && customBadgesData) {
+      setCustomBadges(customBadgesData.getCustomBadges);
+    }
+  }, [customBadgesLoading, customBadgesError, customBadgesData]);
+
+  return (
+    <Flex width="100%" height="fit-content" flexDir="column" gap="15px">
+      <Flex
+        width="100%"
+        height="fit-content"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Flex alignItems="center" gap="15px">
+          <Text textStyle="web.h2" color="primary.700">
+            System Badges
+          </Text>
+          <Text textStyle="web.b3" color="text.light.secondary" marginTop="7px">
+            System badges will be granted to participants automatically.
+          </Text>
+        </Flex>
+      </Flex>
+      <SystemBadgeTable
+        loading={systemBadgesLoading}
+        error={systemBadgesError}
+        badges={systemBadges}
+      />
+      <Flex
+        width="100%"
+        height="fit-content"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Flex alignItems="center" gap="15px">
+          <Text textStyle="web.h2" color="primary.700">
+            Custom Badges
+          </Text>
+          <Text textStyle="web.b3" color="text.light.secondary" marginTop="7px">
+            You can create new and reward participants custom badges.
+          </Text>
+        </Flex>
+        <Flex alignItems="center" gap="15px">
+          <GreenOutlineButton
+            label="Assign Custom Badge"
+            action={() => setAssign(true)}
+            is_active={assign}
+          />
+          <OrangeButton
+            label="Create New"
+            action={() => setCreate(true)}
+            is_active={create}
+          />
+        </Flex>
+      </Flex>
+      <CustomBadgeTable
+        loading={customBadgesLoading}
+        error={customBadgesError}
+        badges={customBadges}
+      />
+
+      {create && <CreateCustomBadgeModal onClose={() => setCreate(false)} />}
+      {assign && <AssignCustomBadgeModal onClose={() => setAssign(false)} />}
+    </Flex>
+  );
+}
--- frontend/src/admin/pages/badges/components/EditCustomBadgeModal.tsx
+++ frontend/src/admin/pages/badges/components/EditCustomBadgeModal.tsx
@@ -1,72 +1,70 @@
 export {};
 // TODO: Refactor this component
-// import React, { useState } from "react";
-// import { useMutation } from "@apollo/client";
-// import { EDIT_CUSTOM_BADGE } from "../../../../gql/mutations";
-// import ModalContainer from "../../../common/form/ModalContainer";
-// import CoreInput from "../../../common/form/CoreInput";
-// 
-// interface EditCustomBadgeModalProps {
-//   onClose: () => void;
-//   selected: any;
-// }
-// 
-// const EditCustomBadgeModal: React.FC<EditCustomBadgeModalProps> = ({
-//   onClose,
-//   selected,
-// }) => {
-//   const [badgeName, setBadgeName] = useState(selected.name);
-//   const [badgeCriteria, setBadgeCriteria] = useState(selected.description);
-//   const [error, setError] = useState("");
-// 
-//   const [editCustomBadge] = useMutation(EDIT_CUSTOM_BADGE);
-// 
-//   const handleSave = async () => {
-//     setError("");
-//     if (!badgeName || !badgeCriteria) {
-//       setError("All fields are required.");
-//       return;
-//     }
-// 
-//     try {
-//       await editCustomBadge({
-//         variables: {
-//           custom_badge_id: selected.badge_id,
-//           new_custom_badge_name: badgeName,
-//           new_custom_badge_description: badgeCriteria,
-//         },
-//       });
-//       localStorage.setItem("notification", "Custom badge updated");
-//       window.location.reload();
-//     } catch (err: any) {
-//       setError(err.message);
-//     }
-//   };
-// 
-//   return (
-//     <ModalContainer
-//       title="Edit Custom Badge"
-//       submit_text="Save Changes"
-//       submit_action={handleSave}
-//       cancel_action={onClose}
-//       error={error}
-//     >
-//       <CoreInput
-//         label="Badge Name"
-//         current_value={badgeName}
-//         action={(e: any) => setBadgeName(e.target.value)}
-//         type="text"
-//         width="350px"
-//       />
-//       <CoreInput
-//         label="Badge Criteria"
-//         current_value={badgeCriteria}
-//         action={(e: any) => setBadgeCriteria(e.target.value)}
-//         type="text"
-//         width="350px"
-//       />
-//     </ModalContainer>
-//   );
-// };
-// 
-// export default EditCustomBadgeModal;
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { UPDATE_CUSTOM_BADGE } from "../../../../gql/customBadgeRequests";
+import PopupContainer from "../../../../ui/containers/PopupContainer";
+import TextInput from "../../../../ui/inputs/TextInput";
+
+interface EditCustomBadgeModalProps {
+  onClose: () => void;
+  selected: any;
+}
+
+const EditCustomBadgeModal: React.FC<EditCustomBadgeModalProps> = ({
+  onClose,
+  selected,
+}) => {
+  const [badgeName, setBadgeName] = useState(selected.name);
+  const [badgeCriteria, setBadgeCriteria] = useState(selected.description);
+  const [error, setError] = useState("");
+
+  const [editCustomBadge] = useMutation(UPDATE_CUSTOM_BADGE);
+
+  const handleSave = async () => {
+    setError("");
+    if (!badgeName || !badgeCriteria) {
+      setError("All fields are required.");
+      return;
+    }
+
+    try {
+      await editCustomBadge({
+        variables: {
+          custom_badge_id: selected.badge_id,
+          new_custom_badge_name: badgeName,
+          new_custom_badge_description: badgeCriteria,
+        },
+      });
+      localStorage.setItem("notification", "Custom badge updated");
+      window.location.reload();
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
+  return (
+    <PopupContainer
+      title="Edit Custom Badge"
+      submit_text="Save Changes"
+      submit_action={handleSave}
+      cancel_action={onClose}
+      error_message={error}
+    >
+      <TextInput
+        label="Badge Name"
+        current_value={badgeName}
+        update_action={(e: any) => setBadgeName(e.target.value)}
+        size="medium"
+      />
+      <TextInput
+        label="Badge Criteria"
+        current_value={badgeCriteria}
+        update_action={(e: any) => setBadgeCriteria(e.target.value)}
+        size="medium"
+      />
+    </PopupContainer>
+  );
+};
+
+export default EditCustomBadgeModal;
